5370f2b9b8f6caa973f37d96b6185ecd36cceed4
kyle33kim <142847569+kyle33kim@users.noreply.github.com>
2024-05-20 18:52:53 -0700

    Create header.js

---
+++ client/src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <div className='Title'>welp</div>
+            <div>
+                <hr></hr>     
+            </div>
+        </>
+    )
+}
+
+export default Header;
